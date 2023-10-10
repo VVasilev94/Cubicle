008d38ec726b914330adfb0780793d97edc13372
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-10-10 12:39:22 +0300

    connect mongoose

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 
 
 const expressConfig = require('./config/expressConfig')
 const handlebarsConfig = require('./config/handlebarsConfig')
+const dbConnect = require('./config/dbConfig')
 const routes = require('./routes')
 
 const app = express()
 const PORT = 5000;
 
+
+dbConnect()
+.then(() => console.log('DB Connected successfully'))
+.catch(err => {
+    console.log('DB error: ', err);
+})
 expressConfig(app)
 handlebarsConfig(app)
 
