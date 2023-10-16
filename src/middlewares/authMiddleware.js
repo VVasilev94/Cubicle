2ebefad7b123996abec9b9946764a762a4e66212
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-10-16 17:12:07 +0300

    add to every created cube the owner, provide middleware for auth

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("../lib/jwt");
+const { SECRET } = require("../constants");
+
+exports.auth = async (req, res, next) => {
+  const token = req.cookies["auth"];
+  if (token) {
+      try {
+        const decodedToken = await jwt.verify(token, SECRET)
+        req.user = decodedToken
+    next();
+
+    } catch (error) {
+        res.clearCookie('auth')
+        res.redirect('/users/login')
+    }
+  } else {
+    next();
+  }
+};
