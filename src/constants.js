7977a8c81f918f858d09fff15bdf9af3712d1050
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-10-16 16:13:40 +0300

    save the cookie with jwt in browser to know that you are logged in

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,7 @@ const constants = {
     PORT: 5000,
     //              localhost = 127.0.0.1
     URL: "mongodb://127.0.0.1:27017/cubicle-may-2023", // Connection String
+    SECRET: 'cc730b2b-894c-4336-80a2-81e553379a01',
   };
   
   module.exports = constants;
