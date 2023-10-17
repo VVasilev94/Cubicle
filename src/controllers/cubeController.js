e0f5984105c16f4f8173c21092b587ff6cacc211
Vasil Vasilev <vasilvasilev944@gmail.com>
2023-10-17 10:18:42 +0300

    update cubes functionality

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -73,4 +73,14 @@ router.get('/:cubeId/delete', async (req, res) => {
   res.render('cube/delete', { cube, options })
 })
 
+router.post('/:cubeId/edit', async (req, res) => {
+  const { cubeId } = req.params
+  const { name, imageUrl, difficultyLevel, description } = req.body
+  const payload = { name, imageUrl, difficultyLevel, description }
+
+  await cubeService.update(cubeId, payload)
+
+  res.redirect(`/cubes/${cubeId}/details`)
+})
+
 module.exports = router;
